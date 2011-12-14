2dce81b48747f7833ff28f57de5bb36e5cde759a
australiensun <australien@sun>
2011-12-14 09:51:06 +0100

    Add basic Options page

--- data/main-wid/tooltip-panel.js
+++ data/main-wid/tooltip-panel.js
@@ -4,6 +4,5 @@ $("a").button();
 $("#button_options").click(function() {
   console.debug("button_options click")
   self.port.emit("show_options");
-  // self.hide(); // TODO: hide after click.
   return false;
 })
