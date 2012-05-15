8d77ce3262aa7072f20ab56de756faa365c902b7
australiensun <australien@sun>
2012-05-15 22:36:24 +0200

    Add TODOs for next steps

--- data/hyperlink-scanner.js
+++ data/hyperlink-scanner.js
@@ -85,7 +85,7 @@ function modifyHyperlink(domHref) {
   $(domHref).after("<a class='myButtonLink'></a>");
   var insertedNode = $(domHref).next();
   insertedNode.attr("href", "#" + origHref); // TODO: on hover show something in status bar to avoid having the #... in the address bar
-  insertedNode.attr("title", "Open in Windows Explorer: " + origHref); // quicktip
+  insertedNode.attr("title", "Open in Windows Explorer: " + origHref); // quicktip; TODO: change this dynamically depending on OS
   insertedNode.attr("alien_OrigHref", origHref); // set new attribute for later in callback
   // insertedNode.css('background-color', 'yellow');
   
