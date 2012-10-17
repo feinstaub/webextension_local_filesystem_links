d3e335851687937900b3e8c9354890e547114483
australiensun <australien@sun>
2012-10-17 16:53:28 +0200

    Fix issue #5: "Prevent Reloading of Page" when working with Plone. (" If you append "/view" to the end of the url, and click the link, it should redirect you to the same url, only without "/view", causing a reload.")

--- data/hyperlink-scanner.js
+++ data/hyperlink-scanner.js
@@ -84,7 +84,14 @@ function modifyHyperlink(domHref) {
   //$(domHref).after(" [<a class='myButtonLink' href='hallo'>hallo</a>]");
   $(domHref).after("<a class='myButtonLink'></a>");
   var insertedNode = $(domHref).next();
-  insertedNode.attr("href", "#" + origHref); // TODO: on hover show something in status bar to avoid having the #... in the address bar
+  
+  // do not do that:
+  //// insertedNode.attr("href", "#" + origHref); // todo: on hover show something in status bar to avoid having the #... in the address bar
+  // because:
+  // 1. http://stackoverflow.com/questions/876390/reliable-cross-browser-way-of-setting-status-bar-text
+  //   "For security reasons, most modern browsers disable status bar access by default."
+  // 2. Fixes https://github.com/feinstaub/firefox_addon_local_filesystem_links/issues/5
+  
   insertedNode.attr("title", "Open in " + initData.fileManagerDisplayName + ": " + origHref); // quicktip; TODO: change this dynamically depending on OS
   insertedNode.attr("alien_OrigHref", origHref); // set new attribute for later in callback
   // insertedNode.css('background-color', 'yellow');
