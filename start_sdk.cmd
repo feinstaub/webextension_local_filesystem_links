8c7970e090f2bddafeca0b24e54e573625e3a96d
australiensun <australien@sun>
2013-06-08 23:05:09 +0200

    start_sdk.cmd: move testprofile one level up

--- start_sdk.cmd
+++ start_sdk.cmd
@@ -2,8 +2,8 @@
 echo HELP
 echo ----
 echo 1) Prepare          # cd %cd%
-echo 2a) Run tests       # cfx test -p $p1
-echo 2b) Run (debug)     # cfx run -p $p1
+echo 2a) Run tests       # cfx test -p ..\TESTPROFILE
+echo 2b) Run (debug)     # cfx run -p ..\TESTPROFILE
 echo 2c) Create xpi file # cfx xpi
 echo ----
 cmd /K ..\addon-sdk-1.14\bin\activate.bat
