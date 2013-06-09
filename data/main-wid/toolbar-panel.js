d1051b9a3285ead601fc43228da3a9e127d2fe55
australiensun <australien@sun>
2013-06-09 17:25:16 +0200

    format code

--- data/main-wid/toolbar-panel.js
+++ data/main-wid/toolbar-panel.js
@@ -1,35 +1,35 @@
 $("a").button();
-        
+
 $("#button_options").click(function() {
-  //// console.log("debug: button_options click")
-  self.port.emit("show_options");
-  return false;
+    //// console.log("debug: button_options click")
+    self.port.emit("show_options");
+    return false;
 });
 
 $("#button_rescan").click(function() {
-  //// console.log("debug: button_options click")
-  self.port.emit("action_rescan_page");
-  return false;
+    //// console.log("debug: button_options click")
+    self.port.emit("action_rescan_page");
+    return false;
 });
 
 self.on("message", function(message) {
-  
-  if (message.type === "fresh_data") {
-    var data = message.data;
-    
-    showElement($("#statusDisabled"), !data.enableScanning);
-    showElement($("#statusInactive"), data.enableScanning && false); // TODO
-    showElement($("#statusActive"), data.enableScanning);
-    
-    $("#versionText").text(data.selfVersion);
-  }
+
+    if (message.type === "fresh_data") {
+        var data = message.data;
+
+        showElement($("#statusDisabled"), !data.enableScanning);
+        showElement($("#statusInactive"), data.enableScanning && false); // TODO
+        showElement($("#statusActive"), data.enableScanning);
+
+        $("#versionText").text(data.selfVersion);
+    }
 });
 
 function showElement(elem, show) {
-  if (show) {
-    elem.css('display', 'inline');
-  }
-  else {
-    elem.hide();
-  }
+    if (show) {
+        elem.css('display', 'inline');
+    }
+    else {
+        elem.hide();
+    }
 }
