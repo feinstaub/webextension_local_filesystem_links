b180616bc63fc3983f161e270a7c0458c016122d
Oscar Reimer <oscar.reimer@reimerbrothers.com>
2015-07-05 13:45:45 +0200

    Fix some deprecated code
    17 out of 18 tests passes
    
    (cherry picked from commit d65b4df430f22ffad848aebef2c5b4a196773f38)
    
    Conflicts:
    	lib/sdk/launch-local-process.js
    	package.json

--- start_sdk.cmd
+++ start_sdk.cmd
@@ -6,5 +6,5 @@ echo 2a) Run tests       # cfx test -p ..\TESTPROFILE
 echo 2b) Run (debug)     # cfx run -p ..\TESTPROFILE
 echo 2c) Create xpi file # cfx xpi
 echo ----
-cmd /K ..\addon-sdk-1.14\bin\activate.bat
+cmd /K ..\addon-sdk-1.17\bin\activate.bat
 pause
