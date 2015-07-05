5ef5fda9be7f638da1fe6d118ec661401e4eab1d
Gregor Mi <codestruct@posteo.org>
2015-07-05 13:34:42 +0200

    switch to sdk v1.17

--- start_sdk.sh
+++ start_sdk.sh
@@ -2,10 +2,9 @@
 
 # call it with source (.)
 # e. g. $ . ./start_sdk.sh
+# see README.md
 
-# see start_sdk.cmd
-
-cd ~/dev/share/addon-sdk-1.14/
+cd ~/dev/share/addon-sdk-1.17/
 source bin/activate
 cd -
 
