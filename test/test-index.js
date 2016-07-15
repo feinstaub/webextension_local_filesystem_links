ef0b8a18eb1d804a35286a4ae600e2eaf7e86ac8
Alexander Wolf <awolf2904@gmail.com>
2016-07-16 00:49:15 +0200

    renamed to match index.js

---
+++ test/test-index.js
@@ -0,0 +1,24 @@
+var main = require( "../index" ),
+    tabs = require( "sdk/tabs" );
+
+let { before, after } = require('sdk/test/utils');
+/*
+// todo add pagemod whitelist testing
+exports[ "test in whitelist" ] = function( assert, done ) {
+    var options = require( "../lib/common/preferences" ).options;
+    console.log('simplePrefs', options);
+    // assert.pass("whitelist done");
+    assert.ok( main.isAttached(), "Attached whitelisted" );
+};
+
+before(exports, function (name, assert, done) {
+  tabs.open( {
+      url: 'http://localhost:3000',
+      onActivate: funcion() {
+          done();
+      }
+
+  });
+});
+*/
+require( "sdk/test" ).run( exports );
