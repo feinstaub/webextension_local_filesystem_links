c6ba23fb23430b1a463896f38044a6c247abbe24
AWolf81 <awolf2904@gmail.com>
2017-01-28 10:46:28 +0100

    Smaller fixes for next release

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
 var eslint = require('gulp-eslint');
  
 var sourceFiles = [
@@ -17,20 +17,22 @@ gulp.task('lint', function() {
     // Otherwise, the task may end before the stream has finished. 
     // return gulp.src(['**/*.js','!node_modules/**'])
     return gulp.src(sourceFiles)
+        .pipe(plumber())
             //'!node_modules/**'])
         // eslint() attaches the lint output to the "eslint" property 
         // of the file object so it can be used by other modules. 
         .pipe(eslint())
         // eslint.format() outputs the lint results to the console. 
         // Alternatively use eslint.formatEach() (see Docs). 
-        .pipe(eslint.format())
+        .pipe(eslint.format());
         // To have the process exit with an error code (1) on 
         // lint error, return the stream and pipe to failAfterError last. 
-        .pipe(eslint.failAfterError());
+        // .pipe(eslint.failAfterError());
 });
 
 gulp.task('watch', function() {
-    return watch(sourceFiles, ['lint']);
+    // return watch(sourceFiles, ['lint']);
+    gulp.watch(sourceFiles, ['lint']);
 });
 
 gulp.task('default', ['lint', 'watch']);
