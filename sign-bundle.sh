a43a46444d2890415cd1ae04a11e6b94232e19e2
AWolf81 <AWolf2904@gmail.com>
2020-04-23 23:49:47 +0200

    add listed channel to sign script

--- sign-bundle.sh
+++ sign-bundle.sh
@@ -9,5 +9,5 @@ fi
 if [[ -n "$unlisted" ]]; then
    web-ext sign --api-key "${AMO_JWT_ISSUER}" --api-secret "${AMO_JWT_SECRET}" --source-dir ./dist --channel=unlisted
 else
-  web-ext sign --api-key "${AMO_JWT_ISSUER}" --api-secret "${AMO_JWT_SECRET}" --source-dir ./dist
+  web-ext sign --api-key "${AMO_JWT_ISSUER}" --api-secret "${AMO_JWT_SECRET}" --source-dir ./dist --channel=listed
 fi
