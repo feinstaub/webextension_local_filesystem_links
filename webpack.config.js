4b92b7d9009f28b19ab45a44156c64df82adee90
AWolf81 <AWolf2904@gmail.com>
2023-05-29 00:03:47 +0200

    fix production build

--- webpack.config.js
+++ webpack.config.js
@@ -147,7 +147,7 @@ module.exports = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports.devtool = ''; // ;'#source-map';
+    module.exports.devtool = 'nosources-source-map';
     // http://vue-loader.vuejs.org/en/workflow/production.html
     module.exports.plugins = (module.exports.plugins || []).concat([
         new webpack.DefinePlugin({
